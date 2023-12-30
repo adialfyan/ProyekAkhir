4b3a02f227e2198ffb8ec157007da6d8d4ed5794
adialfyan <adialfianhafiz@gmail.com>
2023-12-31 04:24:30 +0700

    Proyek Akhir

---
+++ app/Filament/Resources/Shop/ProductResource.php
@@ -0,0 +1,259 @@
+<?php
+
+namespace App\Filament\Resources\Shop;
+
+use App\Filament\Resources\Shop\BrandResource\RelationManagers\ProductsRelationManager;
+use App\Filament\Resources\Shop\ProductResource\Pages;
+use App\Filament\Resources\Shop\ProductResource\RelationManagers;
+use App\Filament\Resources\Shop\ProductResource\Widgets\ProductStats;
+use App\Models\Shop\Product;
+use Filament\Forms;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Form;
+use Filament\Notifications\Notification;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Filters\QueryBuilder;
+use Filament\Tables\Filters\QueryBuilder\Constraints\BooleanConstraint;
+use Filament\Tables\Filters\QueryBuilder\Constraints\DateConstraint;
+use Filament\Tables\Filters\QueryBuilder\Constraints\NumberConstraint;
+use Filament\Tables\Filters\QueryBuilder\Constraints\TextConstraint;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $slug = 'shop/products';
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    protected static ?string $navigationGroup = 'Shop';
+
+    protected static ?string $navigationIcon = 'heroicon-o-bolt';
+
+    protected static ?string $navigationLabel = 'Products';
+
+    protected static ?int $navigationSort = 0;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Group::make()
+                    ->schema([
+                        Forms\Components\Section::make()
+                            ->schema([
+                                Forms\Components\TextInput::make('name')
+                                    ->label('Nama')
+                                    ->required()
+                                    ->maxLength(255)
+                                    ->live(onBlur: true)
+                                    ->afterStateUpdated(function (string $operation, $state, Forms\Set $set) {
+                                        if ($operation !== 'create') {
+                                            return;
+                                        }
+
+                                        $set('slug', Str::slug($state));
+                                    }),
+
+                                Forms\Components\TextInput::make('slug')
+                                    ->disabled()
+                                    ->dehydrated()
+                                    ->required()
+                                    ->maxLength(255)
+                                    ->unique(Product::class, 'slug', ignoreRecord: true),
+
+                                Forms\Components\MarkdownEditor::make('Description')
+                                    ->label('Deskripsi')
+                                    ->columnSpan('full'),
+                            ])
+                            ->columns(2),
+
+                        Forms\Components\Section::make('Image')
+                            ->schema([
+                                SpatieMediaLibraryFileUpload::make('media')
+                                    ->label('Gambar')
+                                    ->collection('product-images')
+                                    ->multiple()
+                                    ->maxFiles(5)
+                                    ->hiddenLabel(),
+                            ])
+                            ->collapsible(),
+
+                        Forms\Components\Section::make('Price')
+                            ->label('Harga')
+                            ->schema([
+                              Forms\Components\TextInput::make('price')
+                                    ->label('Harga')
+                                    ->numeric()
+                                    ->rules(['regex:/^\d{1,6}(\.\d{0,2})?$/'])
+                                    ->required()
+                                    ->prefix('Rp ')
+                                    ->suffix(',-')
+                                    ->label('Harga'),
+                            ])
+                            ->columns(2),
+                        Forms\Components\Section::make('Inventory')
+                            ->schema([
+
+                                Forms\Components\TextInput::make('qty')
+                                    ->label('Kuantitas')
+                                    ->numeric()
+                                    ->rules(['integer', 'min:0'])
+                                    ->required(),
+
+                            ])
+                            ->columns(2),
+
+                        Forms\Components\Section::make('Shipping')
+                            ->schema([
+                                Forms\Components\Checkbox::make('backorder')
+                                    ->label('Produk ini bisa dikembalikan'),
+
+                                Forms\Components\Checkbox::make('requires_shipping')
+                                    ->label('Produk ini bisa dikirimkan'),
+                            ])
+                            ->columns(2),
+                    ])
+                    ->columnSpan(['lg' => 2]),
+
+                Forms\Components\Group::make()
+                    ->schema([
+                        Forms\Components\Section::make('Status')
+                            ->schema([
+                                Forms\Components\DatePicker::make('published_at')
+                                    ->label('Tersedia Pada')
+                                    ->default(now())
+                                    ->required(),
+                            ]),
+
+                        Forms\Components\Section::make('Associations')
+                            ->label('Asosiasi')
+                            ->schema([
+                                Forms\Components\Select::make('shop_brand_id')
+                                    ->relationship('brand', 'name')
+                                    ->searchable()
+                                    ->hiddenOn(ProductsRelationManager::class),
+
+                                Forms\Components\Select::make('categories')
+                                    ->relationship('categories', 'name')
+                                    ->multiple()
+                                    ->required(),
+                            ]),
+                    ])
+                    ->columnSpan(['lg' => 1]),
+            ])
+            ->columns(3);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\SpatieMediaLibraryImageColumn::make('product-image')
+                    ->label('Gambar')
+                    ->collection('product-images'),
+
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Nama')
+                    ->searchable()
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('brand.name')
+                    ->searchable()
+                    ->sortable()
+                    ->toggleable(),
+
+                Tables\Columns\TextColumn::make('price')
+                    ->label('Harga')
+                    ->searchable()
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('qty')
+                    ->label('Kuantitas')
+                    ->searchable()
+                    ->sortable()
+                    ->toggleable(),
+
+                Tables\Columns\TextColumn::make('published_at')
+                    ->label('Publish Date')
+                    ->date()
+                    ->sortable()
+                    ->toggleable()
+                    ->toggledHiddenByDefault(),
+            ])
+            ->filters([
+                QueryBuilder::make()
+                    ->constraints([
+                        TextConstraint::make('name'),
+                        TextConstraint::make('slug'),
+                        TextConstraint::make('description'),
+                        NumberConstraint::make('price')
+                            ->icon('heroicon-m-currency-dollar'),
+                        NumberConstraint::make('qty')
+                            ->label('Quantity'),
+                        BooleanConstraint::make('is_visible')
+                            ->label('Visibility'),
+                        BooleanConstraint::make('featured'),
+                        BooleanConstraint::make('backorder'),
+                        BooleanConstraint::make('requires_shipping')
+                            ->icon('heroicon-m-truck'),
+                        DateConstraint::make('published_at'),
+                    ])
+                    ->constraintPickerColumns(2),
+            ], layout: Tables\Enums\FiltersLayout::AboveContentCollapsible)
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->groupedBulkActions([
+                Tables\Actions\DeleteBulkAction::make()
+                    ->action(function () {
+                        Notification::make()
+                            ->title('Now, now, don\'t be cheeky, leave some records for others to play with!')
+                            ->warning()
+                            ->send();
+                    }),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            RelationManagers\CommentsRelationManager::class,
+        ];
+    }
+
+    public static function getWidgets(): array
+    {
+        return [
+            ProductStats::class,
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getGlobalSearchResultDetails(Model $record): array
+    {
+        /** @var Product $record */
+
+        return [
+            'Brand' => optional($record->brand)->name,
+        ];
+    }
+
+    public static function getGlobalSearchEloquentQuery(): Builder
+    {
+        return parent::getGlobalSearchEloquentQuery()->with(['brand']);
+    }
+}
